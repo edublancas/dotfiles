ed2e7f3723be5f4250360e85c57353356a69bc85
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-12-18 00:17:45 -0600

    bootstrap based on holman

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotfiles
 My dotfiles, just like cool kids do
+
+Based on https://github.com/holman/dotfiles
--- magic.sh
+++
@@ -1,6 +0,0 @@
-#I should automate this...
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/aliases ~/.aliases
-ln -s ~/dotfiles/extra ~/.extra
