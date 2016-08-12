9c89987a00d939ff4f71620fc35d8ffef8af7373
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-08-12 16:47:08 -0500

    updates rprofile

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -7,7 +7,7 @@ export ZSH=/Users/Edu/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="zhann" #dstufft, muse
+ZSH_THEME="zhann" #dstufft, muse, zhann
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
