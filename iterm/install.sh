ce2bbab6e0107dc9e766a56953c16a3db20d30e5
Eduardo Blancas <edu.blancas@gmail.com>
2016-08-23 10:44:02 -0500

    sync sublime and adds gae sdk to path

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,4 +1,5 @@
-cd $ITERM_SOURCE
-make
+cd ~/dev/misc
+git clone https://github.com/jaredculp/iterm2-borderless-padding
+bash iterm2-borderless-padding/install.sh 25 25
 rm -rf /Applications/iTerm2.app/
 mv iTerm2/build/Development/iTerm2.app/ /Applications/iTerm2.app/
