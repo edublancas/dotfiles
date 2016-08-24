dd6811fae4376bfab60379df4761b5494631b578
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2016-08-24 13:54:51 -0500

    updates preferences script

--- setup/preferences.sh
+++ setup/preferences.sh
@@ -2,24 +2,36 @@
 DROPBOX="$HOME/Dropbox/app settings"
 PREFERENCES="$HOME/Library/Preferences"
 
-DIVVY="com.mizage.Divvy.plist"
-# Import settings
-# Alfred
+function setup_files {
+    for file in $( cat config_files.txt ); do
+        cp -vr "$PREFERENCES/$file" "$DROPBOX/$file"
+    done
+}
 
+function sync_files {
+    for file in $( cat config_files.txt ); do
+        ln -sf "$DROPBOX/$file" $PREFERENCES/$file
+    done
+}
 
-if  [[ $1 = "setup" ]]; then
-    echo "Copying your preference files to Dropbox"
-    cp "$PREFERENCES/$DIVVY" $DROPBOX
-elif [[ $1 = "sync" ]]; then
-    echo "Are you sure you want to sync? This will override your settings (yes/no): "
+function confirm {
     read confirmation
     if  [[ $confirmation != "yes" ]]; then
         exit
     fi
-    echo "Copying your preference files from Dropbox"
-    cd $PREFERENCES
-    rm $DIVVY
-    ln -s "$DROPBOX/$DIVVY"
+}
+
+if  [[ $1 = "setup" ]]; then
+    echo "Are you sure you want to copy to Dropbox? This will override the contents (yes/no)"
+    confirm
+    echo "Copying your preference files to Dropbox"
+    setup_files
+    echo 'Done'
+elif [[ $1 = "sync" ]]; then
+    echo "Are you sure you want to sync? This will override your settings (yes/no): "
+    confirm
+    echo "Copying your preferences files from Dropbox"
+    sync_files
     echo 'Done. Log out for the changes to take effect'
 else
     echo 'Invalid option'
