0fd5f2b9b9b4fae8f1d1ce4c2d1d9cc5248d623d
Eduardo Blancas <edu.blancas@gmail.com>
2016-09-01 16:20:16 -0500

    adds quicklook plugins

--- setup/essentials.sh
+++ setup/essentials.sh
@@ -31,6 +31,16 @@ brew cask install little-snitch
 brew cask install caffeine
 brew cask install basictex
 
+# Quicklook plugins
+# https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install qlimagesize
+
 # md editor: typora
 # joe
 # oh-my-zsh
