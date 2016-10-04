42deede8c13e9a07162068bcd0cb74af46027844
Eduardo Blancas <edu.blancas@gmail.com>
2016-10-04 17:41:57 -0500

    updates script to install iterm

--- setup/iterm.sh
+++ setup/iterm.sh
@@ -1,5 +1,6 @@
 cd ~/dev/misc
 git clone https://github.com/jaredculp/iterm2-borderless-padding
-bash iterm2-borderless-padding/install.sh 25 25
-rm -rf /Applications/iTerm2.app/
-mv iTerm2/build/Development/iTerm2.app/ /Applications/iTerm2.app/
+cd iterm2-borderless-padding
+bash install.sh 25 25
+rm -rf /Applications/iTerm.app/
+mv iTerm2/build/Development/iTerm2.app/ /Applications/iTerm.app/
