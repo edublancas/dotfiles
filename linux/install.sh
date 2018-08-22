ac01fa338f8a93065c7b2d46e0520435516410f4
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-08-22 01:39:52 -0400

    adds some setup scripts

---
+++ linux/install.sh
@@ -0,0 +1,5 @@
+# install essentials on a linux machine
+
+sudo apt install update
+sudo apt install -y upgrade
+sudo apt install -y install git htop tree gcc direnv
---
+++ setup/git.sh
@@ -0,0 +1,6 @@
+# https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+git config --global user.name "name"
+git config --global user.email "email"
+
+# https://git-scm.com/docs/git-credential-cache
+git config credential.helper 'cache --timeout=25200'
